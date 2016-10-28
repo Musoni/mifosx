810559d184a02c80f7fe14305885644fe92a1b8f
Sander van der Heijden <sandervanderheyden@musoni.eu>
2016-10-28 11:53:30 +0200

    MS-120 - Adjust MS-120 back to original situation, as it is not used by any tenants and causes problems

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/loanaccount/domain/LoanRepaymentScheduleProcessingWrapper.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/loanaccount/domain/LoanRepaymentScheduleProcessingWrapper.java
@@ -205,14 +205,6 @@ public class LoanRepaymentScheduleProcessingWrapper {
                         && loanCharge.getChargeCalculation().isPercentageBased()) {
                     BigDecimal amount = BigDecimal.ZERO;
                     
-                    // specified due date charges are usually attached to loan installments, therefore:
-                    // totalPrincipal = the loan installment principal
-                    // totalInterest = the loan installment interest
-                    if (loanCharge.isSpecifiedDueDate()) {
-                        totalPrincipal = period.getPrincipal(currency);
-                        totalInterest = period.getInterestCharged(currency);
-                    }
-                    
                     if (loanCharge.getChargeCalculation().isPercentageOfAmountAndInterest()) {
                         amount = amount.add(totalPrincipal.getAmount()).add(totalInterest.getAmount());
                     } else if (loanCharge.getChargeCalculation().isPercentageOfInterest()) {
