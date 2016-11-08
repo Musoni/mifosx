f212f425357f8169d646d10359538b0e863e22b6
Emmanuel Nnaa <emmanuelnnaa@musoni.eu>
2016-11-08 17:45:35 +0100

    Revert "commit for MLITE-1181 (Implement framework for bulk data-exports in Excel)"
    This reverts commit 9e2cb868a3f452e122ccc77c53d847f0ee1a7883.

--- mifosng-provider/src/main/resources/sql/migrations/core_db/V290_9__addNamePropertyToDataExportTable.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `m_data_export` ADD `name` VARCHAR(100) NOT NULL AFTER `id`;
-
-update `m_data_export` set name = concat(base_entity_name, ' data export ', id);
-
-alter table `m_data_export` add constraint unique_name unique (name);
