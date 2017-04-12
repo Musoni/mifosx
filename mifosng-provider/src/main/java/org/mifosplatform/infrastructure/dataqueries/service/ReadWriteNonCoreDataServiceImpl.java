6bcff4eef78a0d1e1808cbcaf4a5a6978b526165
raoulngakam <raoulngakam@gmail.com>
2017-04-12 10:55:23 +0200

    MK-2747 add single quote around string return by formula expression when the column is of type string

--- mifosng-provider/src/main/java/org/mifosplatform/infrastructure/dataqueries/service/ReadWriteNonCoreDataServiceImpl.java
+++ mifosng-provider/src/main/java/org/mifosplatform/infrastructure/dataqueries/service/ReadWriteNonCoreDataServiceImpl.java
@@ -1879,7 +1879,16 @@ public class ReadWriteNonCoreDataServiceImpl implements ReadWriteNonCoreDataServ
 
                 columnName = "`" + key + "`";
                 insertColumns += ", " + columnName;
-                selectColumns += "," + pValueWrite + " as " + columnName;
+
+                if(pColumnHeader.isString()){
+
+                    selectColumns += ", '" + pValueWrite+ "' as " + columnName;
+
+                }else{
+                    selectColumns += "," + pValueWrite + " as " + columnName;
+                }
+
+
             }
             else if (affectedColumns.containsKey(key)) {
             	
