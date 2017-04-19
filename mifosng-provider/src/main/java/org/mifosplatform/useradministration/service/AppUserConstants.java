f5d357bc07f9e39d43d9ccd6ec85f27b6eedc48d
Emmanuel Nnaa <emmanuelnnaa@musoni.eu>
2017-04-19 14:59:46 +0200

    commit for MLITE-729 (Ability to disable a user)

--- mifosng-provider/src/main/java/org/mifosplatform/useradministration/service/AppUserConstants.java
+++ mifosng-provider/src/main/java/org/mifosplatform/useradministration/service/AppUserConstants.java
@@ -10,5 +10,6 @@ public class AppUserConstants {
     public static final String PASSWORD_NEVER_EXPIRES = "passwordNeverExpires";
     public static final String IS_SELF_SERVICE_USER = "isSelfServiceUser";
     public static final String CLIENTS = "clients";
+    public static final String IS_ENABLED = "isEnabled";
 
 }
