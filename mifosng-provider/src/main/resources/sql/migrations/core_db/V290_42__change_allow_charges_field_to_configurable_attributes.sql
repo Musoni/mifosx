7c602fc5d87fa594aa252923f6bd97fe16295d9d
stefan <stefangeers@musoni.eu>
2017-07-18 11:13:04 +0200

    Revert "MLITE-1244: fixed product level charges and added allowAdditionalCharges flag to loan product"
    This reverts commit 2821b5d4bfb8548417e2f975578b33b8e75d6904.

--- mifosng-provider/src/main/resources/sql/migrations/core_db/V290_42__change_allow_charges_field_to_configurable_attributes.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE m_product_loan DROP COLUMN allow_additional_charges;
-
-ALTER TABLE m_product_loan_configurable_attributes ADD COLUMN allow_additional_charges TINYINT(1) DEFAULT '1' NOT NULL;
