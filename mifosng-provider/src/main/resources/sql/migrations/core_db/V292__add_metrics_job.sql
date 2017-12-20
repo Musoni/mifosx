bff0eb85a2f0c5f9aa607c84cdd078d798e8ae0d
Sander van der Heyden <sander@musonisystem.com>
2017-12-20 07:10:32 +0100

    Temp commit to remove faulty DB migrations

--- mifosng-provider/src/main/resources/sql/migrations/core_db/V292__add_metrics_job.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `job` (`name`, `display_name`, `cron_expression`, `create_time`,`job_key`) VALUES ('Calculate dashboard metrics', 'Calculate dashboard metrics', '0 5 0 1/1 * ? *', now(),"Calculate dashboard metricsJobDetail1 _ DEFAULT");
-
